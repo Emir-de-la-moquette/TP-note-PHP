04e14435cce9c2fa85b71d3e246d61167dc4c235
Emir-de-la-moquette <shanky.yt@gmail.com>
2025-01-07 14:30:04 +0100

    start

---
+++ Classes/autoLoader.php
@@ -0,0 +1,12 @@
+<?php
+
+class AutoLoader{
+    static function register(){
+        spl_autoload_register(array(__CLASS__,'autoload'));
+    }
+
+    static function autoload($fqcn){
+        $path = str_replace('\\', '/', $fqcn).'.php';
+        require 'Classes/'.$path;
+    }
+}
