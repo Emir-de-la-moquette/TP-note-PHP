bd4a4a516505185f83e5208c232a219dd00ad447
RICHARD Baptiste <143808981+TronnoxUwU@users.noreply.github.com>
2025-01-10 16:01:28 +0100

    bd faites

--- data/creation.sql
+++ data/creation.sql
@@ -27,21 +27,27 @@ CREATE TABLE REALISER (
     score INT(4),
     PRIMARY KEY (idUtilisateur, idQuiz, date)
 );
+ALTER TABLE REALISER ADD FOREIGN KEY (idUtilisateur) REFERENCES UTILISATEUR(idUtilisateur);
+ALTER TABLE REALISER ADD FOREIGN KEY (idQuiz) REFERENCES QUIZ(idQuiz);
 
 CREATE TABLE CONTENIR (
     idQuiz INT(8),
     idQuestion INT(8),
     PRIMARY KEY (idQuiz,idQuestion)
 );
+ALTER TABLE CONTENIR ADD FOREIGN KEY (idQuestion) REFERENCES QUESTION(idQuestion);
+ALTER TABLE CONTENIR ADD FOREIGN KEY (idQuiz) REFERENCES QUIZ(idQuiz);
 
 CREATE TABLE CORRIGER (
     idQuestion INT(8),
     reponse VARCHAR(16),
     PRIMARY KEY (idQuestion, reponse)
 );
+ALTER TABLE CORRIGER ADD FOREIGN KEY (idQuestion) REFERENCES QUESTION(idQuestion);
 
 CREATE TABLE CHOIX (
     idQuestion INT(8),
     choix VARCHAR(16),
     PRIMARY KEY (idQuestion, reponse)
 );
+ALTER TABLE CHOIX ADD FOREIGN KEY (idQuestion) REFERENCES QUESTION(idQuestion);
