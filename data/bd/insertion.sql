c2852c7b5d0fee4c1a317fe5b77bc2c61372d843
TronnoxUwU <baptisterichard98@free.fr>
2025-01-16 08:39:21 +0100

    ça marche

--- data/bd/insertion.sql
+++ data/bd/insertion.sql
@@ -2,7 +2,7 @@ insert into QUIZ(idQuiz, nomQuiz) values (1,"UwU Quiz"),
                                          (2,"Feur Quiz");
 
 insert into QUESTION(nomQuestion, texte, score, types) values ("FURRY", "Qu'est ce qu'un UwU ?", 10,"radio"),
-                                                          ("Feur", "Quoi", 100,"chekbox");
+                                                          ("Feur", "Quoi", 100,"checkbox");
 
 insert into CONTENIR(idQuiz, nomQuestion) values (1,"FURRY"),
                                                  (1,"Feur"),
